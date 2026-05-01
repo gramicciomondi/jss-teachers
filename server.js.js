b27ed08f064163e0620919b3e6cf0bac1426c790
gramicciomondi <gramicciomondi60@gmail.com>
2026-05-01 17:12:02 +0300

    Add files via upload

--- server.js.js
+++ server.js.js
@@ -17,11 +17,15 @@ app.use(cors());
 app.use(express.static(__dirname));
 
 // ================= DATABASE =================
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect(err => {
